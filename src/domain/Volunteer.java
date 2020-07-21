5a8d333f9dfbe8d2490b98535617dae0d3c58f30
MartinG <martinjavierguilhem88@gmail.com>
2020-07-21 12:58:09 +0200

    Añadí método: - toStringVolunteer Agregué variable description en clase Volunteer

--- src/domain/Volunteer.java
+++ src/domain/Volunteer.java
@@ -3,12 +3,19 @@ package domain;
 public class Volunteer extends AbsStaffMember {
 	
 
+	protected String description;
+
+
 	public Volunteer(String name, String address, String phone, String description) throws Exception {
 		super(name, address, phone);
 		
-		//TODO
+		this.description=description;
 	}
 
+	public String toStringVolunteer() {
+		return "EMPLOYEE: [NAME: " + name + ", ADDRESS: " + address + ", PHONE: " + phone + 
+				", DESCRIPTION: " + description + "]";
+	}
 	
 	@Override
 	public void pay() {
