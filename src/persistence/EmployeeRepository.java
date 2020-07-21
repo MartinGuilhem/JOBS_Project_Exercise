cd35691944cb8951556b8e6a251150700c94086b
MartinG <martinjavierguilhem88@gmail.com>
2020-07-21 10:54:17 +0200

    Agrego una lista de employees en la clase EmployeeRepository adicional a la lista de members

--- src/persistence/EmployeeRepository.java
+++ src/persistence/EmployeeRepository.java
@@ -4,33 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import domain.AbsStaffMember;
+import domain.Employee;
 
 public class EmployeeRepository {
 
-	private static List<AbsStaffMember> members=new ArrayList<>();
-	private static List<AbsStaffMember> employees = new ArrayList<>();
+	private static List<AbsStaffMember> members = new ArrayList<>();
+	private static List<Employee> employees = new ArrayList<>();
 	
 	public EmployeeRepository(){
 	
 	}
 	
+	
 	public List<AbsStaffMember> getAllMembers(){
 		return new ArrayList<>(members);
 	}
-	
-	public List<AbsStaffMember> getAllEmployees(){
-		return new ArrayList<>(employees);
-	}
-	
+		
 	public void addMember(AbsStaffMember member) throws Exception{
 		if(member==null) throw new Exception();
 		members.add(member);
 	}
 	
-	public void addEmployee(AbsStaffMember employee) throws Exception{
+	
+	 
+	public List<Employee> getAllEmployees(){
+		return new ArrayList<>(employees);
+	}
+		
+	public void addEmployee(Employee employee) throws Exception{
 		if(employee==null) throw new Exception();
 		employees.add(employee);
 	}
+	
 	
 	
 }
